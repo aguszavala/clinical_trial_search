9dac1260bc0ac3919a5467ae9423e2615f3c8f7e
aguszavala <azavala1996@gmail.com>
2024-10-08 22:05:17 -0400

    Update search endpoint to return 404 code if no search parameters have been added

--- routes/clinicalTrials.js
+++ routes/clinicalTrials.js
@@ -3,6 +3,12 @@ import {searchClinicalTrials} from '../controllers/clinicalTrialsController.js'
 
 const router = express.Router()
 
-router.post('/search',searchClinicalTrials)
+router.post('/search',(req,res) =>{
+
+    const { disease, city, state} = req.body
+    if (!disease || !city||!state){
+        return res.status(404).json({error: 'Required search parameters are missing'})
+    }
+    searchClinicalTrials(req,res)})
 
 export default router
